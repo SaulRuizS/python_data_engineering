0620ec36fc6f85bce114f8ce5bca8e23370735a4
Saul Ruiz <saul.f.ruiz.silva@gmail.com>
2026-04-17 19:49:50 -0600

    psycopg2 test with f1_database

--- psycopg2_test.py
+++ psycopg2_test.py
@@ -1,43 +1,44 @@
 import psycopg2 as pcg2
 
 connection = pcg2.connect(
-    database = "mydb",
+    database = "f1_database",
     user = "postgres",
     host = 'localhost',
-    password = "********",
+    password = "Mosmostaza1?",
     port = 5432
 )
 
 cursor = connection.cursor()
 
+# cursor.execute("""
+    # --begin-sql
+    # CREATE TABLE postgresql_test_table (
+        # num_id  SERIAL          PRIMARY KEY
+        # ,num    INT
+        # ,info   VARCHAR(255)
+    # );
+# """)
+# 
+# cursor.execute("""
+    # --begin-sql
+    # INSERT INTO postgresql_test_table (
+        # num
+        # ,info
+    # )
+    # VALUES (
+        # %s
+        # ,%s
+    # );
+# """,(7,"Juan P. Montoya F1 Wins")
+# )
+
 cursor.execute("""
     --begin-sql
-    CREATE TABLE postgresql_test_table (
-        num_id  SERIAL          PRIMARY KEY
-        ,num    INT
-        ,info   VARCHAR(255)
-    );
+    SELECT COUNT(*) FROM driver
+    WHERE total_race_wins >= 20;
 """)
 
-cursor.execute("""
-    --begin-sql
-    INSERT INTO postgresql_test_table (
-        num
-        ,info
-    )
-    VALUES (
-        %s
-        ,%s
-    );
-""",(7,"Juan P. Montoya F1 Wins")
-)
-
-cursor.execute("""
-    --begin-sql
-    SELECT * FROM postgresql_test_table;
-""")
-
-cursor.fetchone()
+print(cursor.fetchmany(100))
 
 connection.commit()
 
