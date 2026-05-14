ffe917eebbe5b8bcd27a561350677d5de8a9b922
Saul Ruiz <saul.f.ruiz.silva@gmail.com>
2026-05-13 22:34:02 -0600

    Posgresql practice

--- psycopg2_test.py
+++ psycopg2_test.py
@@ -15,31 +15,36 @@ cursor = connection.cursor()
 cursor.execute("""
     --begin-sql
     SELECT
-        name
-        ,date_of_birth
-        ,date_of_death
-        ,place_of_birth
-        ,total_race_starts
-        ,total_podiums
-        ,total_race_wins
-        ,total_points
-        ,total_pole_positions
-        ,total_fastest_laps
-        ,total_grand_slams
+        season_driver_standing.year
+        ,driver.name
+        --,date_of_birth
+        --,date_of_death
+        --,place_of_birth
+        --,total_race_starts
+        ,driver.total_podiums
+        ,driver.total_race_wins
+        --,total_points
+        ,driver.total_pole_positions
+        --,total_fastest_laps
+        --,total_grand_slams
+        ,season_driver_standing.points
     FROM driver
-    WHERE date_of_birth >= '1996-01-01'
-    ORDER BY date_of_birth ASC;
+    LEFT JOIN season_driver_standing ON driver.id = season_driver_standing.driver_id
+    WHERE driver.date_of_birth >= '1996-01-01'
+        AND driver.total_race_wins > 0
+        AND season_driver_standing.year = 2024
+    ORDER BY season_driver_standing.points DESC;
 """)
 
 data = cursor.fetchall()
 
-df = pd.DataFrame(data, columns=['name', 'date_of_birth', 'date_of_death', 'place_of_birth', 'total_race_starts', 'total_podiums', 'total_race_wins', 'total_points', 'total_pole_positions', 'total_fastest_laps', 'total_grand_slams'])
+df = pd.DataFrame(data, columns=['year', 'name', 'total_podiums', 'total_race_wins', 'total_pole_positions', 'points'])
 
-graph = px.line(df, x="name", y="total_race_wins", title="Podiums per driver")
+#graph = px.line(df, x="name", y="total_race_wins", title="Race Wins per driver")
 
-graph.show()
+#graph.show()
 
-#print(df)
+print(df)
 
 connection.commit()
 
